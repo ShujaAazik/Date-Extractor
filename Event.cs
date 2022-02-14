c7c22ae1365c859167d95926cac7ac43dcd002a8
Suja Aazik <Suja.Aazik@sureservegroup.co.uk>
2022-02-14 10:58:39 +0000

    Added Event Class

---
+++ Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Extractor
+{
+    internal class Event
+    {
+        public string title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string notes { get; set; }
+
+        public bool bunting { get; set; }
+
+        public string date
+        {
+            set { Date = DateTime.Parse(value); }
+        }
+    }
+}
